76b4a0c1792002590218f3803d0afc2c17908be5
DieOver <lennonmilicic@hotmail.com>
2021-09-13 23:16:26 -0300

    update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,8 +2,19 @@
  * NativeScript Polyfills
  */
 
-import "@nativescript/angular/polyfills";
-import "@nativescript/core/globals";
-import "@nativescript/zone-js";
-import "@nativescript/zone-js/dist/pre-zone-polyfills";
-import "zone.js";
+// Install @nativescript/core polyfills (XHR, setTimeout, requestAnimationFrame)
+import '@nativescript/core/globals';
+// Install @nativescript/angular specific polyfills
+import '@nativescript/angular/polyfills';
+
+/**
+ * Zone.js and patches
+ */
+// Add pre-zone.js patches needed for the NativeScript platform
+import '@nativescript/zone-js/dist/pre-zone-polyfills';
+
+// Zone JS is required by default for Angular itself
+import 'zone.js';
+
+// Add NativeScript specific Zone JS patches
+import '@nativescript/zone-js';
